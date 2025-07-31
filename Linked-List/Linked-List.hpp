f5b8efd40eb2cffada8b388b85d3132c32988cf1
Kieroxide <kieranbprivate@gmail.com>
2025-07-31 22:42:13 +0100

    Added << operator for easy print. Also used auto-formatter

--- Linked-List/Linked-List.hpp
+++ Linked-List/Linked-List.hpp
@@ -2,127 +2,143 @@
 #include <cassert>
 #include <iostream>
 
-template<typename Type>
+template <typename Type>
 
-class LinkedList{
-    private:
-        struct Node {
-            Type data;
-            Node* next;
-            Node(const Type& val) : data(val), next(nullptr) {}
-        };
-        Node* head;
-        int size;
-
-        // Helper method for inserting after a node
-        void insertAfter(Node* start, Node* middle){
-            assert(start && middle);
-
-            Node* end = start->next;
-            middle->next = end;
-            start->next = middle;
+class LinkedList
+{
+   private:
+    struct Node
+    {
+        Type data;
+        Node* next;
+        Node(const Type& val) : data(val), next(nullptr) {
         }
+    };
+    Node* head;
+    int size;
 
-        // Helper method to traverse and return node at an index
-        Node* traverseTo(int index) const{
-            assert(index < size && index >= 0);
+    // Helper method for inserting after a node
+    void insertAfter(Node* start, Node* middle) {
+        assert(start && middle);
 
-            int current_index = 0;
-            Node* current = head;
-            while(current_index != index && current->next != nullptr){
-                current_index++;
-                current = current->next;
+        Node* end    = start->next;
+        middle->next = end;
+        start->next  = middle;
+    }
+
+    // Helper method to traverse and return node at an index
+    Node* traverseTo(int index) const {
+        assert(index < size && index >= 0);
+
+        int current_index = 0;
+        Node* current     = head;
+        while (current_index != index && current->next != nullptr) {
+            current_index++;
+            current = current->next;
+        }
+        return current;
+    }
+
+   public:
+    LinkedList() : head(nullptr), size(0) {
+    }
+    // Deconstructor to free all allocated memory
+    ~LinkedList() {
+        Node* current = head;
+        while (current != nullptr) {
+            Node* next = current->next;
+            delete current;
+            current = next;
+        }
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, const LinkedList& list) {
+        os << "[";
+        for (int i = 0; i < list.length(); i++) {
+            os << list[i];
+            if (i < list.length() - 1) {
+                os << ", ";
             }
-            return current;
+        }
+        os << "]";
+        return os;
+    }
+
+    // Get and Sets list[index]
+    Type& operator[](int index) {
+        Node* node = traverseTo(index);
+        return node->data;
+    }
+
+    // Gets const list[index]
+    const Type& operator[](int index) const {
+        Node* node = traverseTo(index);
+        return node->data;
+    }
+
+    int length() const {
+        return size;
+    }
+
+    bool isEmpty() const {
+        return length() == 0;
+    }
+
+    // Method to insert at the end of list
+    void insert(Type data) {
+        insert(data, size);
+    }
+
+    // Method to create and insert node at an index
+    void insert(Type data, int index) {
+        assert(index >= 0 && index <= size);
+        size++;
+        Node* node = new Node(data);
+
+        if (index == 0) {
+            Node* next = head;
+            head       = node;
+            head->next = next;
+            return;
         }
 
-    public:
-        LinkedList() : head(nullptr), size(0){}
-        // Deconstructor to free all allocated memory
-        ~LinkedList(){
-            Node* current = head;
-            while(current != nullptr) {
-                Node* next = current->next;
-                delete current;
-                current = next;
-            }
-        }
+        Node* current = traverseTo(index - 1);
+        insertAfter(current, node);
+    }
 
-        // Get and Sets list[index]
-        Type& operator[](int index){
-            Node* node = traverseTo(index);
-            return node->data;
-        }
+    // Method to remove node at an index
+    Type removeAt(int index) {
+        assert(index >= 0 && index < size);
+        size--;
 
-        // Gets const list[index]
-        const Type& operator[](int index) const{
-            Node* node = traverseTo(index);
-            return node->data;
-        }
-
-        int length() const{
-            return size;
-        }
-
-        bool isEmpty() const{
-            return length() == 0;
-        }
-
-        // Method to insert at the end of list
-        void insert(Type data){
-            insert(data, size);
-        }
-
-        // Method to create and insert node at an index
-        void insert(Type data, int index){
-            assert(index >= 0 && index <= size);
-            size++;
-            Node* node = new Node(data);
-
-            if(index == 0){
-                Node* next = head;
-                head = node;
-                head->next = next;
-                return;
-            }
-            
-            Node* current = traverseTo(index-1);
-            insertAfter(current, node);
-        }
-
-        // Method to remove node at an index
-        Type removeAt(int index){
-            assert(index >= 0 && index < size);
-            size--;
-
-            if(index == 0){
-                Node* next = head->next;
-                Node* oldHead = head;
-                head = next;
-                Type value = oldHead->data;
-                delete oldHead;
-                return value;
-            }
-
-            Node* prev = traverseTo(index - 1);
-            // Links prev to next
-            Node* node = prev->next;
-            Node* next = node->next;
-            prev->next = next;
-
-            Type value = node->data;
-            delete node;
+        if (index == 0) {
+            Node* next    = head->next;
+            Node* oldHead = head;
+            head          = next;
+            Type value    = oldHead->data;
+            delete oldHead;
             return value;
         }
 
-        void clear(){
-            Node* current = head;
-            while(current){
-                Node* next = current->next;
-                delete current;
-                current = next;
-            }
-            size = 0;
-            head = nullptr;
+        Node* prev = traverseTo(index - 1);
+        // Links prev to next
+        Node* node = prev->next;
+        Node* next = node->next;
+        prev->next = next;
+
+        Type value = node->data;
+        delete node;
+        return value;
+    }
+
+    void clear() {
+        Node* current = head;
+        while (current) {
+            Node* next = current->next;
+            delete current;
+            current = next;
         }
+        size = 0;
+        head = nullptr;
+    }
 };
--- Linked-List/test.cpp
+++ Linked-List/test.cpp
@@ -1,29 +1,31 @@
+#include <cassert>
+#include <iostream>
+
 #include "Linked-List.hpp"
 
-#include <iostream>
-#include <cassert>
 
-int main(){
+int main() {
     LinkedList<int> test;
-
+    
     // Insert tests
     int size = 4;
-    for(int i = 0; i < size; i++){ 
+    for (int i = 0; i < size; i++)
+    {
         test.insert(i);
         assert(test[i] == i);
     }
     
     test.insert(10, test.length());
     assert(test[test.length() - 1] == 10);
-
+    
     test.insert(-1, 0);
     test.insert(-2, 2);
     assert(test.length() == 7);
     assert(test[0] == -1 && test[2] == -2);
-    //ALL INSERTION TEST PASSED
+    // ALL INSERTION TEST PASSED
 
     // Assignment tests
-    test[0] = 100;
+    test[0]                 = 100;
     test[test.length() - 1] = 5;
     assert(test[0] == 100 && test[test.length() - 1] == 5);
     // ASSIGNMENT TESTS PASSED
@@ -32,15 +34,16 @@ int main(){
     test[1] = 5;
     test.removeAt(0);
     assert(test[0] == 5);
-    
+
     test[test.length() - 2] = 25;
-    test.removeAt(test.length()-1);
+    test.removeAt(test.length() - 1);
     assert(test[test.length() - 1] == 25);
     assert(test.length() == 5);
 
-    for(int i = 0; i < test.length(); i++){
+    for (int i = 0; i < test.length(); i++)
+    {
         std::cout << "test[" << i << "]: " << test[i] << std::endl;
     }
     // REMOVE TESTS PASSED
-    
+
 }
