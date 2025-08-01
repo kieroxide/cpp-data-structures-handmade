6fda03da71b048a16186a6c41698beab53576644
Kieroxide <kieranbprivate:gmail.com>
2025-08-01 01:11:40 +0100

    Updated to use renamed Vector methods

--- Stack/test.cpp
+++ Stack/test.cpp
@@ -8,23 +8,23 @@ int main() {
 
     // Initially empty
     assert(stack.isEmpty());
-    assert(stack.length() == 0);
+    assert(stack.getSize() == 0);
 
     // Push 1 element
     stack.push(10);
     assert(!stack.isEmpty());
-    assert(stack.length() == 1);
+    assert(stack.getSize() == 1);
     assert(stack.peek() == 10);  // peek does not remove
 
     // Push more elements
     stack.push(20);
     stack.push(30);
-    assert(stack.length() == 3);
+    assert(stack.getSize() == 3);
     assert(stack.peek() == 30);
 
     // Pop elements, check order
     assert(stack.pop() == 30);
-    assert(stack.length() == 2);
+    assert(stack.getSize() == 2);
     assert(stack.peek() == 20);
 
     assert(stack.pop() == 20);
