fd0aea28a62e7ff2dec508bb11ceb7b9e7a2d720
Kieroxide <kieranbprivate:gmail.com>
2025-08-01 01:11:24 +0100

    Updated to use renamed Vector methods

--- Stack/Stack.hpp
+++ Stack/Stack.hpp
@@ -17,23 +17,23 @@ class Stack
     // Pop item from top of the stack
     Type pop() {
         assert(!isEmpty());
-        Type item = data.remove(data.length() - 1);
+        Type item = data.remove(data.getSize() - 1);
         return item;
     }
 
     // Return item at top of stack without removing it from the stack
     Type peek() const {
         assert(!isEmpty());
-        return data[data.length() - 1];
+        return data[data.getSize() - 1];
     }
 
     // Returns a true if stack is empty
     bool isEmpty() const {
-        return data.length() == 0;
+        return data.getSize() == 0;
     }
 
     // Returns the amount of items in the stack
-    int length() const {
-        return data.length();
+    int getSize() const {
+        return data.getSize();
     }
 };
