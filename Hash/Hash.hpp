b5705e196067d123780ba8f530e90e4a65912257
Kieroxide <kieranbprivate@gmail.com>
2025-08-01 15:26:54 +0100

    Added a calculate Index private method Added getSize and is empty methods Added contains and containedAt methods and refactored other methods to contain them Fixed bug where count was incremented after checking load_factor in put

--- Hash/Hash.hpp
+++ Hash/Hash.hpp
@@ -16,11 +16,16 @@ class Hash
 
     // Method to resize the hash table to double the size
     void resize() {
+        // Gets all key-value pairs
         auto pairs      = getAllPairs();
         int oldCapacity = buckets.getCapacity();
+
+        // Clear and resize buckets array
         buckets.clear();
         buckets.resize(oldCapacity * 2);
         count = 0;
+
+        // Recalculate index's for each pair
         for (int i = 0; i < pairs.getSize(); i++) {
             put(pairs[i].key, pairs[i].value);
         }
@@ -55,6 +60,11 @@ class Hash
         return static_cast<size_t>(key < 0 ? -key : key);
     }
 
+    // Converts key to Hashmap index
+    size_t calculateIndex(const int key) const{
+        return hashFunction(key) % buckets.getCapacity();
+    }
+
    public:
     Hash() : buckets(INITIAL_CAPACITY), count(0) {
         buckets.resize(INITIAL_CAPACITY);
@@ -72,41 +82,84 @@ class Hash
         return os;
     }
 
+    // Returns total amount of key-value pairs in hashmap
+    int getSize() const{
+        return count;
+    }
+
+    // Returns true if hashmap is empty
+    bool isEmpty() const{
+        return count == 0;
+    }
+
     // Adds key and value to hashmap
     void put(int key, Type value) {
-        int index    = hashFunction(key) % buckets.getCapacity();
-        auto& bucket = buckets[index];
-        for (int i = 0; i < bucket.getSize(); i++) {
-            if (bucket[i].key == key) {
-                bucket[i].value = value;
-                return;
-            }
-        }
-        bucket.insert(Pair<int, Type>(key, value));
-        float loadFactor = static_cast<float>(count) / buckets.getCapacity();
+        // Locates key's index
+        int index        = calculateIndex(key);
+        auto& bucket     = buckets[index];
+        int bucket_index = containedAt(key, bucket);
 
+        // If found update to new value
+        if (bucket_index >= 0) {
+            bucket[bucket_index].value = value;
+            return;
+        } else {
+            bucket.insert(Pair<int, Type>(key, value));
+            count++;
+        }
+
+        // Resize if ratio limit of count to capacity is exceeded
+        float loadFactor = static_cast<float>(count) / buckets.getCapacity();
         if (loadFactor > 0.75f) {
             resize();
         }
-        count++;
+    }
+
+    // Removes element with inputted key returns true if found
+    bool remove(int key) {
+        int index        = calculateIndex(key);
+        auto& bucket     = buckets[index];
+        int bucket_index = containedAt(key, bucket);
+        // If key found remove key and value
+        if (bucket_index >= 0) {
+            bucket.remove(bucket_index);
+            count--;
+            return true;
+        }
+        return false;
+    }
+
+    // Returns boolean whether hashmap contains a value for a key
+    bool contains(const int key) const{
+        int index        = calculateIndex(key);
+        auto& bucket     = buckets[index];
+        return containedAt(key, bucket) >= 0;
+    }
+
+    // Returns the index at which a key is located in a bucket. Else returns -1
+    int containedAt(const int key, const LinkedList<Pair<int, Type>>& bucket) const {
+        for (int i = 0; i < bucket.getSize(); i++) {
+            if (bucket[i].key == key) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     // Get and Set method using []
     Type& operator[](const int key) {
-        int index    = hashFunction(key) % buckets.getCapacity();
-        auto& bucket = buckets[index];
-        for (int i = 0; i < bucket.getSize(); i++) {
-            if (bucket[i].key == key) {
-                return bucket[i].value;
-            }
+        int index        = calculateIndex(key);
+        auto& bucket     = buckets[index];
+        int bucket_index = containedAt(key, bucket);
+
+        if (bucket_index >= 0) {
+            return bucket[bucket_index].value;
         }
+
         // if not found inserts it
         put(key, Type());
-        auto& newBucket = buckets[hashFunction(key) % buckets.getCapacity()];
-        for (int i = 0; i < newBucket.getSize(); i++) {
-            if (newBucket[i].key == key) {
-                return newBucket[i].value;
-            }
-        }
+        auto& newBucket = buckets[calculateIndex(key)];  // Recalcuate index in case of resizing
+        bucket_index    = containedAt(key, newBucket);
+        return newBucket[bucket_index].value;
     }
 };
