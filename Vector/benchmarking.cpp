4cec0212e81b9d52a690ffbc5260ce73ad171c1a
Kieroxide <kieranbprivate@gmail.com>
2025-08-05 20:29:47 +0100

    Created benchmark for remove. Which is the most inefficent method

--- Vector/benchmarking.cpp
+++ Vector/benchmarking.cpp
@@ -2,7 +2,7 @@
 #include <chrono>
 
 template <typename Func>
-void benchmark(Func function, uint32_t iterations = 10000){
+void benchmark(const Func function, const size_t iterations = 10000){
     auto t1 = std::chrono::high_resolution_clock::now();
     for(int i = 0; i < iterations; i++){
         function();
@@ -14,7 +14,7 @@ void benchmark(Func function, uint32_t iterations = 10000){
 }
 
 int main(){
-    uint32_t iterations = 10000000;
+    size_t iterations = 9999999;
 
     std::string a = "Hello";
     std::string b = "World";
@@ -23,8 +23,11 @@ int main(){
     benchmark([&]() {v.append(a);}, iterations); // Append with reallocs 228ns
     v.clear();
 
-    v.ensureCapacity(10000000);
+    v.ensureCapacity(iterations);
     benchmark([&]() {v.append(a);}, iterations); // Append with reserved space 29ns
 
+    iterations = 10000;
+    v.resize(iterations);
+    benchmark([&]() {v.remove(0);}, iterations); // Remove at worst case 247,843ns
     return 0;
 }
