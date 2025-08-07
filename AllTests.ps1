b0df5ea9eabfec4cc58284c27a778ca2b01dc7ce
Kieroxide <kieranbprivate@gmail.com>
2025-08-07 17:37:39 +0100

    Created a powershell file to run all tests on the data structures in one

---
+++ AllTests.ps1
@@ -0,0 +1,28 @@
+cd Vector
+g++ test.cpp -o test.exe
+./test.exe
+cd ..
+cd Stack
+g++ test.cpp -o test.exe
+./test.exe
+cd ..
+cd Queue
+g++ test.cpp -o test.exe
+./test.exe
+cd ..
+cd Pair
+g++ test.cpp -o test.exe
+./test.exe
+cd..
+cd Linked-List
+g++ test.cpp -o test.exe
+./test.exe
+cd..
+cd Hash
+g++ test.cpp -o test.exe
+./test.exe
+cd..
+cd Deque
+g++ test.cpp -o test.exe
+./test.exe
+cd..
