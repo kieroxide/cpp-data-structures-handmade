3cc9fe2ab60b648ab1f663bf98105f059a18be8d
Kieroxide <kieranbprivate:gmail.com>
2025-08-07 18:05:41 +0100

    Updated test script to include dLinkedList

--- AllTests.ps1
+++ AllTests.ps1
@@ -1,28 +1,32 @@
 cd Vector
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
 cd ..
 cd Stack
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
 cd ..
 cd Queue
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
 cd ..
 cd Pair
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
 cd..
 cd Linked-List
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
 cd..
 cd Hash
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
 cd..
 cd Deque
 g++ test.cpp -o test.exe
-./test.exe
+.\test.exe
+cd..
+cd DoublyLinkedList
+g++ test.cpp -o test.exe
+.\test.exe
 cd..
